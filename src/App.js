29b2b102826e4b9f1abb105214e45c95cfd5c14f
PraJha <abcpr108@gmail.com>
2020-11-20 13:14:25 +0530

    Files updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Counters from './components/counters';
+import Navbar from './components/navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        counters: [
+            { id: 1, value: 0 },
+            { id: 2, value: 0 },
+            { id: 3, value: 0 },
+            { id: 4, value: 0 },
+            { id: 5, value: 0 },
+        ]
+    }
+
+    handleReset = () => {
+        const counters = this.state.counters.map((c) => { c.value = 0; return c; });
+        this.setState({ counters })
+    };
+
+    addCounter = () => {
+        const countersId = this.state.counters.map((c) => (c.id)).sort((a, b) => (a - b));
+        const newId = countersId.length > 0 ? countersId[countersId.length - 1] + 1 : 1;
+        const newObj = { id: newId, value: 0 }
+        const counters = [...this.state.counters, newObj]
+        this.setState({ counters })
+    };
+
+    handleIncrement = (counter) => {
+        const counters = this.state.counters.filter((c) => (
+            c.id === counter.id ? c.value = c.value + 1 : c
+        ))
+        this.setState({ counters })
+    }
+
+    handleDecrement = (counter) => {
+        const counters = this.state.counters.filter((c) => {
+            if (c.id === counter.id) {
+                c.value = c.value - 1;
+                if (c.value < 0) {
+                    c.value = 0
+                }
+            }
+            return c
+        })
+        this.setState({ counters })
+    }
+
+    handleDelete = (id) => {
+        const counters = this.state.counters.filter((counter) => {
+            return counter.id !== id;
+        })
+        this.setState({ counters })
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar totalCounters={this.state.counters.filter(c => c.value > 0).reduce((accumulator, c) => { return accumulator + c.value }, 0)} />
+                <div style={{ textAlign: 'center' }}>
+                    <main className="container">
+                        <Counters counter={this.state.counters} onAdd={this.addCounter} onDecrement={this.handleDecrement} onDelete={this.handleDelete} onIncrement={this.handleIncrement} onReset={this.handleReset} />
+                    </main>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
